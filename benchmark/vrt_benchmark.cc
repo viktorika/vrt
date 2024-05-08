7d96c1b10db160d85d32194110119e20a0f34707
Weiqiang Wu <598855847@qq.com>
2024-05-08 17:02:26 +0800

    optimize benchmark code (#9)
    * ignore compile_commands.json
    
    * no upload compile_commands
    
    * add memory debug option
    
    * add file header
    
    * max key size set to 2^20
    
    * delete comment
    
    * improve README
    
    * add annotations
    
    * test func set acquire memory order
    
    * find child add acquire barrier
    
    * optimize benchmark code
    
    ---------
    
    Co-authored-by: weiqiangwu <weiqiangwu@tencent.com>

--- benchmark/vrt_benchmark.cc
+++ benchmark/vrt_benchmark.cc
@@ -1,14 +1,14 @@
 /*
- * @Author: viktorika 
- * @Date: 2024-04-05 19:01:54 
- * @Last Modified by:   viktorika 
- * @Last Modified time: 2024-04-05 19:01:54 
+ * @Author: viktorika
+ * @Date: 2024-04-05 19:01:54
+ * @Last Modified by:   viktorika
+ * @Last Modified time: 2024-04-05 19:01:54
  */
-#include "benchmark/benchmark.h"
 #include <unistd.h>
 #include <random>
 #include <thread>
 #include <utility>
+#include "benchmark/benchmark.h"
 #include "phmap.h"
 #include "vrt.h"
 #include "vrt_node.h"
@@ -16,7 +16,6 @@
 uint32_t kKeySize = 1000000;
 uint32_t kKeyLength = 20;
 std::vector<std::string> keys(kKeySize);
-constexpr uint32_t thread_num = 5;
 
 static int GenKeys() {
   std::random_device rd;
@@ -31,19 +30,25 @@ static int GenKeys() {
   return 0;
 }
 
-static void RunInsertDeConstructPhmapByMutex(benchmark::State& state) {
+static void RunInsertPhmapByMutex(benchmark::State& state) {
+  phmap::parallel_flat_hash_map<std::string, std::string, phmap::priv::hash_default_hash<std::string>,
+                                phmap::priv::hash_default_eq<std::string>,
+                                std::allocator<std::pair<const std::string, std::string>>, 8, std::mutex>
+      map;
+  for (int i = 0; i < kKeySize; i++) {
+    map[keys[i]] = "123";
+  }
   for (auto _ : state) {
-    phmap::parallel_flat_hash_map<std::string, std::string, phmap::priv::hash_default_hash<std::string>,
-                                  phmap::priv::hash_default_eq<std::string>,
-                                  std::allocator<std::pair<const std::string, std::string>>, 8, std::mutex>
-        map;
-    auto func = [&](int start, int end) {
-      for (int i = start; i < end; i++) {
-        map.try_emplace(keys[i], "123");
-      }
-    };
+    state.PauseTiming();
+    auto thread_num = state.range(0);
     std::vector<std::thread> ts(thread_num);
     auto batch = kKeySize / thread_num;
+    state.ResumeTiming();
+    auto func = [&](int start, int end) {
+      for (int i = start; i < end; i++) {
+        map[keys[i]] = "123";
+      }
+    };
     for (int i = 0; i < thread_num; i++) {
       ts[i] = std::thread(func, i * batch, (i + 1) * batch);
     }
@@ -53,16 +58,23 @@ static void RunInsertDeConstructPhmapByMutex(benchmark::State& state) {
   }
 }
 
-static void RunInsertDeConstructVrt(benchmark::State& state) {
+static void RunInsertVrt(benchmark::State& state) {
+  vrt::Vrt<std::string, true, 8> vrt;
+  for (int i = 0; i < kKeySize; i++) {
+    vrt.Upsert(keys[i], "123");
+  }
   for (auto _ : state) {
-    vrt::Vrt<std::string, true, thread_num> vrt;
+    state.PauseTiming();
+    auto thread_num = state.range(0);
+    std::vector<std::thread> ts(thread_num);
+    auto batch = kKeySize / thread_num;
+    state.ResumeTiming();
+
     auto func = [&](int start, int end) {
       for (int i = start; i < end; i++) {
         vrt.Upsert(keys[i], "123");
       }
     };
-    std::vector<std::thread> ts(thread_num);
-    auto batch = kKeySize / thread_num;
     for (int i = 0; i < thread_num; i++) {
       ts[i] = std::thread(func, i * batch, (i + 1) * batch);
     }
@@ -82,14 +94,17 @@ static void RunFindPhmapByMutex(benchmark::State& state) {
   }
   std::vector<std::string> values(kKeySize);
   for (auto _ : state) {
+    state.PauseTiming();
+    auto thread_num = state.range(0);
+    std::vector<std::thread> ts(thread_num);
+    auto batch = kKeySize / thread_num;
+    state.ResumeTiming();
     auto func = [&](int start, int end) {
       for (int i = start; i < end; i++) {
         map.if_contains(keys[i],
                         [&values, i](const std::pair<std::string, std::string>& val) { values[i] = val.second; });
       }
     };
-    std::vector<std::thread> ts(thread_num);
-    auto batch = kKeySize / thread_num;
     for (int i = 0; i < thread_num; i++) {
       ts[i] = std::thread(func, i * batch, (i + 1) * batch);
     }
@@ -100,19 +115,22 @@ static void RunFindPhmapByMutex(benchmark::State& state) {
 }
 
 static void RunFindVrt(benchmark::State& state) {
-  vrt::Vrt<std::string, true, thread_num> vrt;
+  vrt::Vrt<std::string, true, 8> vrt;
   for (int i = 0; i < kKeySize; i++) {
     vrt.Upsert(keys[i], "123");
   }
   std::vector<std::string> values(kKeySize);
   for (auto _ : state) {
+    state.PauseTiming();
+    auto thread_num = state.range(0);
+    std::vector<std::thread> ts(thread_num);
+    auto batch = kKeySize / thread_num;
+    state.ResumeTiming();
     auto func = [&](int start, int end) {
       for (int i = start; i < end; i++) {
         vrt.Find(keys[i], &values[i]);
       }
     };
-    std::vector<std::thread> ts(thread_num);
-    auto batch = kKeySize / thread_num;
     for (int i = 0; i < thread_num; i++) {
       ts[i] = std::thread(func, i * batch, (i + 1) * batch);
     }
@@ -124,20 +142,23 @@ static void RunFindVrt(benchmark::State& state) {
 
 static void RunDeletePhmapByMutex(benchmark::State& state) {
   for (auto _ : state) {
+    state.PauseTiming();
+    auto thread_num = state.range(0);
+    std::vector<std::thread> ts(thread_num);
+    auto batch = kKeySize / thread_num;
     phmap::parallel_flat_hash_map<std::string, std::string, phmap::priv::hash_default_hash<std::string>,
                                   phmap::priv::hash_default_eq<std::string>,
                                   std::allocator<std::pair<const std::string, std::string>>, 8, std::mutex>
         map;
+    for (int i = 0; i < kKeySize; i++) {
+      map.try_emplace(keys[i], "123");
+    }
+    state.ResumeTiming();
     auto func = [&](int start, int end) {
-      for (int i = start; i < end; i++) {
-        map.try_emplace(keys[i], "123");
-      }
       for (int i = start; i < end; i++) {
         map.erase(keys[i]);
       }
     };
-    std::vector<std::thread> ts(thread_num);
-    auto batch = kKeySize / thread_num;
     for (int i = 0; i < thread_num; i++) {
       ts[i] = std::thread(func, i * batch, (i + 1) * batch);
     }
@@ -149,17 +170,20 @@ static void RunDeletePhmapByMutex(benchmark::State& state) {
 
 static void RunDeleteVrt(benchmark::State& state) {
   for (auto _ : state) {
-    vrt::Vrt<std::string, true, thread_num> vrt;
+    state.PauseTiming();
+    auto thread_num = state.range(0);
+    std::vector<std::thread> ts(thread_num);
+    auto batch = kKeySize / thread_num;
+    vrt::Vrt<std::string, true, 8> vrt;
+    for (int i = 0; i < kKeySize; i++) {
+      vrt.Upsert(keys[i], "123");
+    }
+    state.ResumeTiming();
     auto func = [&](int start, int end) {
-      for (int i = start; i < end; i++) {
-        vrt.Upsert(keys[i], "123");
-      }
       for (int i = start; i < end; i++) {
         vrt.Delete(keys[i]);
       }
     };
-    std::vector<std::thread> ts(thread_num);
-    auto batch = kKeySize / thread_num;
     for (int i = 0; i < thread_num; i++) {
       ts[i] = std::thread(func, i * batch, (i + 1) * batch);
     }
@@ -169,12 +193,12 @@ static void RunDeleteVrt(benchmark::State& state) {
   }
 }
 
-BENCHMARK(RunInsertDeConstructPhmapByMutex);
-BENCHMARK(RunInsertDeConstructVrt);
-BENCHMARK(RunFindPhmapByMutex);
-BENCHMARK(RunFindVrt);
-BENCHMARK(RunDeletePhmapByMutex);
-BENCHMARK(RunDeleteVrt);
+BENCHMARK(RunInsertPhmapByMutex)->RangeMultiplier(2)->Range(1, 8);
+BENCHMARK(RunInsertVrt)->RangeMultiplier(2)->Range(1, 8);
+BENCHMARK(RunFindPhmapByMutex)->RangeMultiplier(2)->Range(1, 8);
+BENCHMARK(RunFindVrt)->RangeMultiplier(2)->Range(1, 8);
+BENCHMARK(RunDeletePhmapByMutex)->RangeMultiplier(2)->Range(1, 8);
+BENCHMARK(RunDeleteVrt)->RangeMultiplier(2)->Range(1, 8);
 
 int main(int argc, char** argv) {
   GenKeys();
